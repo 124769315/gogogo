cad5831fd192ff188854bb36cbecdca360488c32
124769315 <124769315@qq.com>
2022-01-08 08:54:51 +0800

    v3

---
+++ 2.1/httpserver.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", myHandler)
+	mux.HandleFunc("/healthz", healthz)
+	http.ListenAndServe(":80", mux)
+}
+
+func healthz(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "200\n")
+}
+
+func myHandler(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "********** request header **********\n")
+	for k, v := range r.Header {
+		io.WriteString(w, fmt.Sprintf("%s=%s\n", k, v))
+	}
+}
